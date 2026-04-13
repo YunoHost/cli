1fe9bac6cec0e8cb8b6f1b528315598a1030cb20
Félix Piédallu <felix@piedallu.me>
2026-04-13 12:54:07 +0200

    logging: call basicConfig to set level

--- src/yunohost_cli/__init__.py
+++ src/yunohost_cli/__init__.py
@@ -14,6 +14,7 @@ from .server import Server
 def set_logging_level_from_int(value: int) -> None:
     levels = [logging.WARNING, logging.INFO, logging.DEBUG]
     level = levels[max(0, min(value, len(levels) - 1))]
+    logging.basicConfig(level=level)
     logging.getLogger().setLevel(level)
 
 
